7d8951ae85e8c05297252bf081dda0f7e6e532c9
khodary94 <khodary94@aucegypt.edu>
2015-12-04 17:28:12 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Vision_project
+//
+//  Created by Mahmoud Khodary on 12/1/15.
+//  Copyright (c) 2015 Mahmoud Khodary. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
